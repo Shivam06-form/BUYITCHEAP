9f2612f3cfc8e2807cb877160b95b606ef3b6d5a
Shivam06-form <shivambhagwat06071998@gmail.com>
2021-11-29 21:07:31 +0530

    Updates

--- precache-manifest.e655f1300aa474b55e25b9f0c635fa72.js
+++ precache-manifest.e655f1300aa474b55e25b9f0c635fa72.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6b79d41a75b7e0a66286",
+    "revision": "a8691a78aa7d74545ee3",
     "url": "/buyitcheap/static/css/main.2ae9a18e.chunk.css"
   },
   {
-    "revision": "6b79d41a75b7e0a66286",
-    "url": "/buyitcheap/static/js/main.02cc42a1.chunk.js"
+    "revision": "a8691a78aa7d74545ee3",
+    "url": "/buyitcheap/static/js/main.edce4b2d.chunk.js"
   },
   {
     "revision": "596d8d4fe214e9bf5626",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/buyitcheap/static/js/2.ef39b42f.chunk.js"
   },
   {
-    "revision": "2701970dc75438433a9da8185425d498",
+    "revision": "646a4791353dfdd888da2e15eab140fc",
     "url": "/buyitcheap/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/buyitcheap/precache-manifest.66f1a5233ee5de477466a003e752c46b.js"
+  "/buyitcheap/precache-manifest.e655f1300aa474b55e25b9f0c635fa72.js"
 );
 
 workbox.clientsClaim();
