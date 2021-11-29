e83770955783a197c87140433c47cf8906d87550
Shivam06-form <shivambhagwat06071998@gmail.com>
2021-11-29 16:57:00 +0530

    Updates

---
+++ precache-manifest.068402622a81c8fee9a8641c388b5493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0376fe62a29957c455b",
+    "url": "/%20BUYITCHEAP/static/css/main.2ae9a18e.chunk.css"
+  },
+  {
+    "revision": "a0376fe62a29957c455b",
+    "url": "/%20BUYITCHEAP/static/js/main.4efcc73f.chunk.js"
+  },
+  {
+    "revision": "37957cb34705e39dc1ec",
+    "url": "/%20BUYITCHEAP/static/js/runtime~main.ceaa2c20.js"
+  },
+  {
+    "revision": "79522a0423b57c4e29ab",
+    "url": "/%20BUYITCHEAP/static/css/2.c169221f.chunk.css"
+  },
+  {
+    "revision": "79522a0423b57c4e29ab",
+    "url": "/%20BUYITCHEAP/static/js/2.2e569fe4.chunk.js"
+  },
+  {
+    "revision": "6ba28890ad166d4ba6c8eaddeeb7084b",
+    "url": "/%20BUYITCHEAP/index.html"
+  }
+];
